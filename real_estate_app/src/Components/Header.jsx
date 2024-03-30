bb6d45ba84d97149cbc955d20e46cbcfa171b668
Emma Paris <osanu.andrei@yahoo.com>
2024-03-30 21:36:20 +0200

    MZ

--- real_estate_app/src/Components/Header.jsx
+++ real_estate_app/src/Components/Header.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 
 export default function Header() {
     const location = useLocation();
     const navigate = useNavigate();
-
+    const auth = getAuth();
+    const [pageState,setPageState] = useState('Sign in'); // default page , like we just entered, we will have Sign In top right
+    useEffect(()=> {
+        onAuthStateChanged(auth, (user)=> { // if user is authenticated , setpagestate to 'Profile' otherwise set it to default (Sign in)
+            if(user) {
+                setPageState('Profile');
+            }
+            else{
+                setPageState('Sign in');
+            }
+        })
+    }, [auth]
+    )
+    
     console.log("Current Path:", location.pathname); // Log current pathname
 
     function matchRoute(route) {
@@ -25,12 +39,18 @@ export default function Header() {
                 </div> 
                 <div>
                 <ul className='flex space-x-10 text-lg'>  
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]  ${matchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate('/')}>Home</li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]  ${matchRoute("/offers") && "text-black border-b-red-500"} `} onClick={() => navigate('/offers')}>Offers</li>
-                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]  ${matchRoute("/sign-in") && "text-black border-b-red-500"} }`} onClick={() => navigate('/sign-in')}>Sign in</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]  
+                        ${matchRoute("/") && "text-black border-b-red-500"}`} onClick={() => navigate('/')}>Home</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px]  
+                        ${matchRoute("/offers") && "text-black border-b-red-500"} `} onClick={() => navigate('/offers')}>Offers</li>
+                        <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] 
+                         ${(matchRoute("/sign-in") || matchRoute("/profile")) && "text-black border-b-red-500"}`} onClick={() => navigate('/profile')}> 
+                            {pageState}
+                            </li>
                     </ul>
                 </div>
             </header>
+            
         </div>
     );
 }
--- real_estate_app/src/Pages/Profile.jsx
+++ real_estate_app/src/Pages/Profile.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
 import { useNavigate } from 'react-router';
 import { useEffect } from 'react';
+import {toast} from 'react-toastify';
+import { doc, getDoc, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
+import { db } from '../firebase';
 const Profile = () => {
   const navigate = useNavigate();
+  const [changeEdit, setChangeEdit] = useState(false); // this is for Edit button, work with states to display different messages, also for enable/disable the ability for users to write in the name input
   const auth = getAuth();
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({ // controll the inputs
     name: '',
     email: ''
   });
@@ -25,6 +29,33 @@ const Profile = () => {
     auth.signOut();
     navigate("/");
   }
+
+  function onChange(e) {
+    setFormData((prevState)=> ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+
+    }))
+
+  }
+  async function onSubmit() {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        //update display name in firebase db
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //update name in firestore 
+        const docRef = doc(db, "users", auth.currentUser.uid) // we make changes in the database using docref and doc from firestore and we display the "name" coming from the currentuser.displayname
+        await updateDoc(docRef, {
+          name,
+        })
+      }
+      toast.success("Profile updated!")
+    } catch (error) {
+      toast.error("Could not update the profile details!")
+    }
+  }
   return (
     <> 
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>  
@@ -32,15 +63,21 @@ const Profile = () => {
         <div className='w-full md:w-[50%] mt-6 px-3'>
           <form>
             { /* Name Input */ }
-            <input type='text' id='name' value={name} disabled className='mb-5 w-full px-3 py-2 text-xl text-gray-800 bg-white border border-gray-300 rounded transition ease-in-out'></input>
+            
+            <input type='text' id='name' value={name} disabled={!changeEdit} onChange = {onChange}className= {`mb-5 w-full px-3 py-2 text-xl text-gray-800 bg-white
+             border border-gray-300 rounded transition ease-in-out ${changeEdit && "bg-red-200 focus:bg-red-200"}`} >  
+              </input> 
 
             { /* Email input */ }
 
             <input type='email' id='email' value={email} disabled className='w-full px-3 py-2 text-xl text-gray-800 bg-white border border-gray-300 rounded transition ease-in-out'></input>
             <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
               <p className='flex items-center mb-6'>Do you want to change your name ?
-                <span className='text-blue-600 hover:text-red-700 transition ease-in-out duration-300 ml-1 cursor-pointer'>
-                  Edit
+                <span
+                onClick={() => {changeEdit && onSubmit();  // with onSubmit we want to add changes to the database and change the state of the input by clicking "Edit" Button
+                  setChangeEdit((prevState) => !prevState)}}
+                className='text-blue-600 hover:text-red-700 transition ease-in-out duration-300 ml-1 cursor-pointer'>
+                  {changeEdit ? "Apply changes" : "Edit"}
                 </span>
               </p>
               <p onClick = {onLogout}className='text-gray-600 hover:text-black transition duration-200 ease-in-out cursor-pointer'>
